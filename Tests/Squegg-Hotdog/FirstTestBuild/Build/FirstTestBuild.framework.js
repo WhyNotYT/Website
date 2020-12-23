9b5bdd6f07a4da7ce54faa39e544a5383a0a74bc
Yumish R. Niroula <yumishniroula2002@gmail.com>
2020-12-23 23:22:23 +0545

    Pause Menu and bug fixes

--- Tests/Squegg-Hotdog/FirstTestBuild/Build/FirstTestBuild.framework.js
+++ Tests/Squegg-Hotdog/FirstTestBuild/Build/FirstTestBuild.framework.js
@@ -1296,7 +1296,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2477456;
+STATICTOP = STATIC_BASE + 2477920;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3234,7 +3234,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2477456;
+var STATIC_BUMP = 2477920;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15015,8 +15015,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 51143;
-Module["wasmMaxTableSize"] = 51143;
+Module["wasmTableSize"] = 51146;
+Module["wasmMaxTableSize"] = 51146;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
