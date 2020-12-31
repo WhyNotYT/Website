f507d2e8c3e4ac6c4d1a0ab35a2e9f4fff155931
Yumish R. Niroula <yumishniroula2002@gmail.com>
2020-12-31 15:55:56 +0545

    Arch Boy Fix

--- Tests/Chimpvine/Arch-boy/Build/Arch-boy.framework.js
+++ Tests/Chimpvine/Arch-boy/Build/Arch-boy.framework.js
@@ -1296,7 +1296,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3462080;
+STATICTOP = STATIC_BASE + 3464208;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3234,7 +3234,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3462080;
+var STATIC_BUMP = 3464208;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15773,8 +15773,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 105175;
-Module["wasmMaxTableSize"] = 105175;
+Module["wasmTableSize"] = 105177;
+Module["wasmMaxTableSize"] = 105177;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
