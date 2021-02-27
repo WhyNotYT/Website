93abdbb2b65d71125aa44a9823e0f133402c0b39
Yumish R. Niroula <yumishniroula2002@gmail.com>
2021-02-28 00:49:26 +0545

    hotdog Aspect ratio test

--- Tests/Squegg-Hotdog/FirstTestBuild/Build/FirstTestBuild.framework.js
+++ Tests/Squegg-Hotdog/FirstTestBuild/Build/FirstTestBuild.framework.js
@@ -1296,7 +1296,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2481984;
+STATICTOP = STATIC_BASE + 2482640;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3234,7 +3234,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2481984;
+var STATIC_BUMP = 2482640;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
